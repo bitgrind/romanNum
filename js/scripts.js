bf52934abd921a2e9aea8c44aae0ceb260109521
Chance Magno and Keith Stedman <chance+keith@>
2017-02-23 16:08:28 -0800

    rough day... couldn't quite get the roman numrals to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,42 @@ $(function(){
   var romanArray = [[1000,'M'],[500,'D'],[100,'C'],[50,'L'],[10,'X'],[5,'V'],[1,'I']];
 
   function findFactor(number){
-    console.log("Factor Start Number: "+number);
+    if(number == 0){
+      return false;
+    }
     $(romanArray).each(function(x){
-      //x = index
       var factorNumber = this[0];
       var factorNumberArray = romanArray[x][0];
-      // console.log("factorNumber: "+factorNumber);
-      // console.log("factorNumberArray: "+factorNumberArray);
       var factorChar = romanArray[x][1];
-      //console.log('index: '+romanArray[0][0]+", number: "+number+", this: "+this[0]);
       if((number/romanArray[x][0]) == 1){
-        console.log('even number'+number/romanArray[x][0]);
         $("#outputNumber h2").append(factorChar);
       }
       if((number/romanArray[x][0]) > 1 && (number/romanArray[x-1][0]) < 1) {
-        console.log("found the highest factor: "+factorNumber);
-        console.log("highest factor divison: "+number/factorNumber);
         var highestFactor = (number/factorNumber);
         var factor = (number/factorNumber);
-        console.log('factor amount'+factor);
+        var factorDecRemainder = (number%factorNumber);
         var factorRemainder = factor.toString();
         var beforeDecimalString = factorRemainder.split('.')[0];
         var afterDecimalString = factorRemainder.split('.')[1];
-        console.log("Before Decimal: "+beforeDecimalString +", After Decimal: "+afterDecimalString);
         var remainderHighestFactor = (number-this);
-        if(beforeDecimalString == 4){
-          console.log('use subtraction: factor: '+romanArray[x-1][0]);
-          var newFactorRemainder = (romanArray[x][0])-romanArray[x-1][0];
-          $("#outputNumber h2").append(romanArray[x][1]+romanArray[x-1][1]);
-        }else{
-          for(i=0;i < beforeDecimalString; i++){
-          console.log(factorChar);
-          $("#outputNumber h2").append(factorChar);
-          }
-        }
         var newRemainder = number - (beforeDecimalString * factorNumber);
-        console.log(newRemainder);
-        findFactor(newRemainder);
+        console.log("number: "+number+", factor"+factor+", newRemainder: "+newRemainder+", factorDecRemainder: "+factorDecRemainder);
+        if(factor == 1.8){
+          console.log("FOUND A 9!!! Index: "+romanArray[x][1]+", aboveIndex: "+romanArray[x-1][1]+", belowIndex: "+romanArray[x+1][1]);
+          $("#outputNumber h2").append(romanArray[x+1][1]+romanArray[x-1][1]);
+        }else if(beforeDecimalString == 4){
+          console.log("R == than 4");
+          console.log("newRemainder: "+newRemainder);
+          $("#outputNumber h2").append(romanArray[x][1]+romanArray[x-1][1]);
+          findFactor(newRemainder);
+        }else if(beforeDecimalString < 4){
+          console.log("R less than 4");
+          console.log("number: "+number+", factor"+factor+", newRemainder: "+newRemainder);
+          for(i=0;i < beforeDecimalString; i++){
+            $("#outputNumber h2").append(factorChar);
+          }
+          findFactor(newRemainder);
+        }
       }
     });
     $("#output h2").append(number);
@@ -48,7 +47,7 @@ $(function(){
   $("#romanNumeral form").submit(function(){
     event.preventDefault();
     var number = parseInt($('#input').val());
+    $("#outputNumber h2").html("");
     findFactor(number);
-    //$("#outputNumber h2").text("");
   });
 });
